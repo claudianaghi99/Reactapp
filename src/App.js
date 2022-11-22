5db3e69f59dc5daf2964d98cef8088cf7c70fbc2
claudianaghi99 <claudia.naghi@principal33.com>
2022-11-23 01:39:25 +0200

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from  'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NoFound from './NoFound';
 
 function App() {
 
    return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch> 
+            {/* //only one route shows at one time ------one route for one page (we have now 1 page) */}
+            <Route exact path="/">
+              <Home/>   
+            </Route>
+            <Route exact path="/create">
+              <Create/>
+            </Route>
+            <Route exact path="/blogs/:id">
+              <BlogDetails/>
+            </Route>
+            <Route path="*">
+              <NoFound/>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
