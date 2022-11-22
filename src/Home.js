fbd8cd806610a0583c621bfb5f877bf64d131c60
claudianaghi99 <claudia.naghi@principal33.com>
2022-11-23 01:39:56 +0200

    Updated home component

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const { data: blogs, isPending, error } = useFetch("http://localhost:8000/blogs");
 
-  
-  const handleDelete= (id) => {
-    
-  }
+  // dependency array un useeffect - no loop
 
-  
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" />
-      <BlogList blogs={blogs.filter((blog)=> blog.author==='mario')} title="marios blogs"/>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
+      {/* //conditional templating in react logical and - if false it doesnt bother  */}
+      {/* //with the right . left-null---false -- when blogs = true, evaluates right and shows data */}
     </div>
   );
 };
